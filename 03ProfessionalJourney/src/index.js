caaf476e39f8bbf5d9b120af7621b24c95d6406b
Melu-Sahu <melusahu0143.s@gmail.com>
2024-02-16 12:44:58 +0530

    utils code discussion here

--- 03ProfessionalJourney/src/index.js
+++ 03ProfessionalJourney/src/index.js
@@ -1,38 +1,33 @@
 // require('dotenv').config({path: './env'});
 
-
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import express from "express";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
-
-
-
-
+  path: "./env",
+});
 
 connectDB()
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running");
+    });
+    app.on("error", (err) => {
+      console.log("err", err);
+    });
+  })
+  .catch((error) => {
+    console.log("MOngodb Connection Failed!!", error);
+  });
 
 const app = express();
 
-
-
-
-
-
-
-
 /* First Approach */
-
-
 
 // ;(async () => {
 //   try {
